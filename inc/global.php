7b6945c8322fc0ab3fd622c1c75bd66336d9751c
Adam Privette <aprivette@gmail.com>
2017-06-19 15:09:20 -0400

    Globally disable related videos in YouTube embeds

--- inc/global.php
+++ inc/global.php
@@ -13,6 +13,18 @@ add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
 });
 
+// IronStrap Options styles
 add_action('admin_enqueue_scripts', function () {
     wp_enqueue_style('admin', get_template_directory_uri().'/admin/style.css');
 });
+
+// Globally disable related videos in YouTube embeds
+function ironstrap_remove_youtube_rel($code){
+    if(strpos($code, 'youtu.be') !== false || strpos($code, 'youtube.com') !== false){
+        $return = preg_replace("@src=(['\"])?([^'\">\s]*)@", "src=$1$2&rel=0", $code);
+        return $return;
+    }
+    return $code;
+}
+add_filter('embed_handler_html', 'ironstrap_remove_youtube_rel');
+add_filter('embed_oembed_html', 'ironstrap_remove_youtube_rel');
