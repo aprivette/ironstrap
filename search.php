9baf7832f87dc4f652d74b6d58438e7c666003d4
Adam Privette <aprivette@gmail.com>
2018-01-04 14:58:53 -0500

    refactored

--- search.php
+++ search.php
@@ -11,42 +11,47 @@
 
  get_header(); ?>
 
-    <section id="site-main-wrapper" class="row" role="main">
+    <section id="site-main-wrapper" class="container" role="main">
         
         <?php if ( have_posts() ) : ?>
             
-        <div class="container cf">
+        <div class="row">
+
+            <div class="col-md-12">
+                
+                <h1>Search Results for: <?php echo '<span>' . esc_html( get_search_query() ) . '</span>'; ?></h1>
+
+                <?php
+                // Start the loop.  
+                while ( have_posts() ) : the_post();
+
+                    /**
+                     * Run the loop for the search to output the results.
+                     * If you want to overload this in a child theme then include a file
+                     * called content-search.php and that will be used instead.
+                     */
+                    get_template_part( 'template-parts/search/content', 'search' );
+
+                // End the loop.
+                endwhile;
+
+                // Previous/next page navigation.
+                the_posts_pagination( array(
+                    'prev_text'          => __( 'Previous' ),
+                    'next_text'          => __( 'Next' ),
+                ) );
             
-            <h1>Search Results for: <?php echo '<span>' . esc_html( get_search_query() ) . '</span>'; ?></h1>
-
-        <?php
-        // Start the loop.  
-        while ( have_posts() ) : the_post();
-
-            /**
-             * Run the loop for the search to output the results.
-             * If you want to overload this in a child theme then include a file
-             * called content-search.php and that will be used instead.
-             */
-            get_template_part( 'template-parts/search/content', 'search' );
-
-        // End the loop.
-        endwhile;
-
-        // Previous/next page navigation.
-        the_posts_pagination( array(
-            'prev_text'          => __( 'Previous' ),
-            'next_text'          => __( 'Next' ),
-        ) );
-    
-        // If no content, include the "No posts found" template.
-        else :
+                // If no content, include the "No posts found" template.
+                else :
+                    
             
-    
-        endif;
-        ?>
+                endif;
+                ?>
+
+            </div>
 
         </div>
+
     </section><!-- .content-area -->
     
 <?php get_footer(); ?>
