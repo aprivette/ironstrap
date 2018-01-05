d39f44454e86ba36123b85158a884142fb8b0ff2
Adam Privette <aprivette@gmail.com>
2018-01-05 18:26:36 -0500

    CSV import for redirects

--- inc/acf_redirects.php
+++ inc/acf_redirects.php
@@ -16,13 +16,40 @@ add_action('template_redirect', function ()
         global $post;
 
         foreach ($redirects as $redirect) {
-            if ($redirect['page'] == $post->ID) {
-                if ($redirect['external']) {
-                    wp_redirect($redirect['redirect_url'], $redirect['redirect_type']);
-                } else {
-                    wp_redirect(get_permalink($redirect['redirect_page']), $redirect['redirect_type']);
-                }
+            $uri = '/' . get_page_uri($post->ID);
+            $uri_slash = $uri . '/';
+            if ($redirect['path'] == $uri || $redirect['path'] == $uri_slash) {
+                wp_redirect($redirect['redirect_url'], $redirect['redirect_type']);
             }
         }
     }
 });
+
+// CSV import to IronStrap Redirects
+add_filter('acf/save_post', function ($post_id)
+{
+    $field_obj = get_field_object('redirect_import', 'option');
+    $key = $field_obj['key'];
+    
+    if(empty($_POST['acf'][$key])) {
+        return;
+    }
+    
+    $field = $_POST['acf'][$key];
+    delete_field('redirect_import' , 'option');
+    $handle = fopen(get_attached_file($field), 'r');
+    
+    while(($data = fgetcsv($handle)) !== false) {
+        if(in_array(null, $data)) {
+            continue;
+        }
+        $row = [
+            'path' => $data[0],
+            'redirect_type' => $data[1],
+            'redirect_url' => $data[2]
+        ];
+        add_row('redirects', $row, $post_id);
+    }
+    
+    return $post_id;
+}, 10, 1);
